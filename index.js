54f76a0f9f5808790675ee67ea4b44deaf644282
Siddhant Grover <siddhantgroverboss@gmail.com>
2021-06-07 12:32:49 +0530

    razorpay

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
-const express = require('express')
-const Razorpay = require('razorpay')
+const express = require("express");
+const Razorpay = require("razorpay");
 
-let  app  =express()
+let app = express();
 
-app.set('view engine','ejs')
-// app.use(express.urlencoded({extended:false}))
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-let KEY_ID= "rzp_test_qRDRpOoJR0OGFp"
-let KEY_SECRET = "pOCYTUxxeoTKVJxENaLMbmj2"
-const razorpay = new Razorpay({//initailize razorpay 
-    key_id:KEY_ID,
-    key_secret:KEY_SECRET
-})
+let KEY_ID = "YOUR_KEY_ID";
+let KEY_SECRET = "YOUR SECRET";
+const razorpay = new Razorpay({
+  //initailize razorpay
+  key_id: KEY_ID,
+  key_secret: KEY_SECRET,
+});
 
-app.get('/',(req,res)=>{
-    res.render('razorpay.ejs',{KEY_ID:KEY_ID})
-})
+app.get("/", (req, res) => {
+  res.render("razorpay.ejs", { KEY_ID: KEY_ID });
+});
 
+app.post("/order", (req, res) => {
+  //make a req from client and client recives an order_id
 
-app.post('/order',(req,res)=>{ //make a req from client and client recives an order_id
+  let options = {
+    amount: 500 * 100, // amount in the smallest currency unit
+    currency: "INR",
+    //receipt: "order_rcptid_11"//we can use an unique idd from npm (like uuid), and generate a recipt everytime you create an order
+  };
+  razorpay.orders.create(options, function (err, order) {
+    //create order from the server , specifies the amt u gonna pay and currency (non mutable)
+    res.json(order);
+  });
+});
 
-    let options = {
-        amount: 500*100,  // amount in the smallest currency unit
-        currency: "INR",
-        //receipt: "order_rcptid_11"//we can use an unique idd from npm (like uuid), and generate a recipt everytime you create an order 
-      };
-      razorpay.orders.create(options, function(err, order) {    //create order from the server , specifies the amt u gonna pay and currency (non mutable)
-        console.log(order);
-        res.json(order)
-      });
-})
+app.post("/is-order-completed", (req, res) => {
+  // console.log(req.body); //you can collect  "razorpay_payment_id" ,"razorpay_order_id","razorpay_signature" from body and use them to verify the signature and Make provisions to store these fields on your server/db.
 
-app.listen(4000,()=>{
-    console.log("started")
-})
+  razorpay.payments.fetch(req.body.razorpay_payment_id).then((paymentDoc) => {
+    if (paymentDoc.status == "captured") {
+      console.log("success");
+      res.send("Payment success");
+    } else {
+      //res.redirect('/')
+    }
+  });
+});
+app.listen(4000, () => {
+  console.log("started at 4000");
+});
